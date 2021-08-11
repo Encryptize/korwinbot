d222076ef089c3827ddd7fd08575cf215a326f47
Jakub Rolecki <46298681+Encryptize@users.noreply.github.com>
2021-08-11 03:22:50 +0200

    Dodanie kodu do repozytorium

---
+++ bot.py
@@ -0,0 +1,39 @@
+import logging, argparse
+from uuid import uuid4
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent
+
+import korwin
+
+# get bot token
+parser = argparse.ArgumentParser(description="Bot auth")
+parser.add_argument('bot_token', type=str, help='Telegram bot token')
+args = parser.parse_args()
+
+# setup bot
+bot = Bot(token=args.bot_token)
+dp = Dispatcher(bot)
+
+# enable logging
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+@dp.inline_handler()
+async def inline_handler(update: InlineQuery):
+    korwin_response = korwin.korwin_random()
+
+    item = InlineQueryResultArticle(
+        id=str(uuid4()),
+        title=korwin_response,
+        input_message_content=InputTextMessageContent(korwin_response)
+    )
+
+    await bot.answer_inline_query(update.id, results=[item], cache_time=1)
+
+@dp.message_handler(commands=['korwin'])
+async def command_handler(update: types.Message):
+    await update.answer(korwin.korwin_random())
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
